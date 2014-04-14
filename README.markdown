0f554ca9636390d22764613cd01631037ecbd359
mattbrailsford <me@mattbrailsford.com>
2014-04-14 14:24:23 +0100

    Initial code commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+OEmbed Picker
+===================
+
+An OEmbed Picker property editor for Umbraco 7
---
+++ Src/Our.Umbraco.OEmbedPicker/App_Plugins/OEmbedPicker/js/oEmbedPicker.js
@@ -0,0 +1,16 @@
+﻿angular.module("umbraco").controller("Our.Umbraco.PropertyEditors.OEmbedPicker",
+    function ($scope, dialogService) {
+
+        $scope.remove = function () {
+            $scope.model.value = "";
+        };
+
+        $scope.add = function() {
+            dialogService.embedDialog({
+                callback: function(data) {
+                    $scope.model.value = data;
+                }
+            });
+        };
+
+    });
